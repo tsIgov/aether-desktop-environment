77efc38d22ac2b762f79d46c5408fee58c89422f
Tsvetan Igov <ts.igov@outlook.com>
2025-01-05 22:16:50 +0200

    Added bluetooth support

---
+++ modules/system/bluetooth.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+	hardware.bluetooth = {
+		enable = true;
+		powerOnBoot = true;
+		settings = {
+			General = {
+				Experimental = true;
+			};
+		};
+	};
+
+	environment.systemPackages = with pkgs; [
+		overskride
+	];
+}
