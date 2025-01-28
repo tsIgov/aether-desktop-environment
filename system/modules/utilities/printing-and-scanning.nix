6f95838814d98eab624d1d26cacdddb0e563250a
Tsvetan Igov <ts.igov@outlook.com>
2025-01-28 13:12:15 +0200

    Improved printing

--- system/modules/utilities/printing-and-scanning.nix
+++ system/modules/utilities/printing-and-scanning.nix
@@ -1,12 +1,26 @@
  { pkgs, ... }:
 
 {
-	services.printing.enable = true;
+	services.printing = {
+		enable = true;
+		drivers = with pkgs; [ gutenprint cups-filters hplipWithPlugin ];
+		browsing = true;
+		browsedConf = ''
+			BrowseDNSSDSubTypes _cups,_print
+			BrowseLocalProtocols all
+			BrowseRemoteProtocols all
+			CreateIPPPrinterQueues All
+
+			BrowseProtocols all
+		'';
+	};
 	services.avahi = {
 		enable = true;
 		nssmdns4 = true;
 		openFirewall = true;
 	};
+
+	
 
 	hardware.sane.enable = true;
 
