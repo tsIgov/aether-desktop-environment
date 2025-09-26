bfb22425151bd8e6fb21fc1d76906eb2f1b85f67
Tsvetan Igov <ts.igov@outlook.com>
2025-09-26 18:50:26 +0300

    Wakeup devices can now be configured

--- modules/system/utilities/cli.nix
+++ modules/system/utilities/cli.nix
@@ -2,6 +2,7 @@
 {
 	environment.systemPackages = with pkgs; [
 		pciutils # CLI for controlling PCI devices
+		usbutils # CLI for controlling USB devices
 		sysstat # Performance monitoring CLI
 		jq # JSON processor used in many of the aether scripts
 		socat # CLI for interacting with sockets
