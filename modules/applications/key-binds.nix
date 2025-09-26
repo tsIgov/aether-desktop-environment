a555ad13bc293fa670748d1dfa2c0b6de85db00b
Tsvetan Igov <ts.igov@outlook.com>
2025-09-26 18:49:36 +0300

    Menus are now triggered by walker and not elephant

--- modules/applications/key-binds.nix
+++ modules/applications/key-binds.nix
@@ -4,8 +4,8 @@
 		wayland.windowManager.hyprland = {
 			extraConfig = ''
 				bindr = SUPER, SUPER_L, exec, walker
-				bind = SUPER, Slash, exec, elephant m aether
-				bind = CTRL ALT, Delete, exec, elephant m power
+				bind = SUPER, Slash, exec, walker -m menus:aether
+				bind = CTRL ALT, Delete, exec, walker -m menus:power
 
 				bind = SUPER, T, exec, ${pkgs.kitty}/bin/kitty
 				bind = SUPER, E, exec, ${pkgs.nemo}/bin/nemo
