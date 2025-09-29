6baba5086874f36b1f100697f641ce988d30737b
Tsvetan Igov <ts.igov@outlook.com>
2025-09-29 19:08:41 +0300

    Small change to the layout

--- modules/window-manager/layout.nix
+++ modules/window-manager/layout.nix
@@ -7,15 +7,18 @@
 					layout = "master";
 				};
 				master = {
-					mfact = 0.7;
+					mfact = 0.5;
 					new_status = "slave";
 					inherit_fullscreen = false;
 					allow_small_split = false;
+					# orientation = "center";
+					# slave_count_for_center_master = 4;
+					# center_master_fallback = "left";
+
 				};
 				group = {
 					insert_after_current = false;
 				};
-
 				workspace = [
 					"special:quick-1, gapsout:100"
 					"special:quick-2, gapsout:100"
