26ee432a13eb51c22445a17ab23369f8ae524119
Tsvetan Igov <ts.igov@outlook.com>
2025-09-30 13:21:55 +0300

    The window manager now lets the scripts choose their own shell

--- modules/window-manager/key-binds.nix
+++ modules/window-manager/key-binds.nix
@@ -8,16 +8,16 @@
 				bind = SUPER, w, movefocus, u
 				bind = SUPER, s, movefocus, d
 
-				binde = SUPER SHIFT, a, exec, sh /etc/aether/window-manager/scripts/move.sh l
-				binde = SUPER SHIFT, d, exec, sh /etc/aether/window-manager/scripts/move.sh r
-				binde = SUPER SHIFT, w, exec, sh /etc/aether/window-manager/scripts/move.sh u
-				binde = SUPER SHIFT, s, exec, sh /etc/aether/window-manager/scripts/move.sh d
+				binde = SUPER SHIFT, a, exec, /etc/aether/window-manager/scripts/move.sh l
+				binde = SUPER SHIFT, d, exec, /etc/aether/window-manager/scripts/move.sh r
+				binde = SUPER SHIFT, w, exec, /etc/aether/window-manager/scripts/move.sh u
+				binde = SUPER SHIFT, s, exec, /etc/aether/window-manager/scripts/move.sh d
 
-				bind = SUPER, 1, exec, sh /etc/aether/window-manager/scripts/change-workspace.sh 1
-				bind = SUPER, 2, exec, sh /etc/aether/window-manager/scripts/change-workspace.sh 2
-				bind = SUPER, 3, exec, sh /etc/aether/window-manager/scripts/change-workspace.sh 3
-				bind = SUPER, 4, exec, sh /etc/aether/window-manager/scripts/change-workspace.sh 4
-				bind = SUPER, 5, exec, sh /etc/aether/window-manager/scripts/change-workspace.sh 5
+				bind = SUPER, 1, exec, /etc/aether/window-manager/scripts/change-workspace.sh 1
+				bind = SUPER, 2, exec, /etc/aether/window-manager/scripts/change-workspace.sh 2
+				bind = SUPER, 3, exec, /etc/aether/window-manager/scripts/change-workspace.sh 3
+				bind = SUPER, 4, exec, /etc/aether/window-manager/scripts/change-workspace.sh 4
+				bind = SUPER, 5, exec, /etc/aether/window-manager/scripts/change-workspace.sh 5
 
 				bind = SUPER SHIFT, 1, movetoworkspace, 1
 				bind = SUPER SHIFT, 2, movetoworkspace, 2
@@ -25,8 +25,8 @@
 				bind = SUPER SHIFT, 4, movetoworkspace, 4
 				bind = SUPER SHIFT, 5, movetoworkspace, 5
 
-				bind = SUPER, Q, exec, sh /etc/aether/window-manager/scripts/quick-toggle.sh
-				bind = SUPER SHIFT, Q, exec, sh /etc/aether/window-manager/scripts/quick-move.sh
+				bind = SUPER, Q, exec, /etc/aether/window-manager/scripts/quick-toggle.sh
+				bind = SUPER SHIFT, Q, exec, /etc/aether/window-manager/scripts/quick-move.sh
 
 
 				# Monitors
@@ -49,10 +49,10 @@
 				# Resizing
 				bind = SUPER SHIFT, r, submap, resize
 				submap = resize
-				binde = SUPER SHIFT, d, exec, sh /etc/aether/window-manager/scripts/resize.sh r
-				binde = SUPER SHIFT, a, exec, sh /etc/aether/window-manager/scripts/resize.sh l
-				binde = SUPER SHIFT, s, exec, sh /etc/aether/window-manager/scripts/resize.sh d
-				binde = SUPER SHIFT, w, exec, sh /etc/aether/window-manager/scripts/resize.sh u
+				binde = SUPER SHIFT, d, exec, /etc/aether/window-manager/scripts/resize.sh r
+				binde = SUPER SHIFT, a, exec, /etc/aether/window-manager/scripts/resize.sh l
+				binde = SUPER SHIFT, s, exec, /etc/aether/window-manager/scripts/resize.sh d
+				binde = SUPER SHIFT, w, exec, /etc/aether/window-manager/scripts/resize.sh u
 				bind = SUPER, d, movefocus, r
 				bind = SUPER, a, movefocus, l
 				bind = SUPER, w, movefocus, u
@@ -69,8 +69,8 @@
 				bindm = SUPER, mouse:273, resizewindow
 
 				# Grouping
-				bind = SUPER, g, exec, sh /etc/aether/window-manager/scripts/group.sh
-				bind = SUPER SHIFT, g, exec, sh /etc/aether/window-manager/scripts/master.sh
+				bind = SUPER, g, exec, /etc/aether/window-manager/scripts/group.sh
+				bind = SUPER SHIFT, g, exec, /etc/aether/window-manager/scripts/master.sh
 				bind = SUPER, tab, changegroupactive, f
 				bind = SUPER SHIFT, tab, changegroupactive, b
 
@@ -78,7 +78,7 @@
 
 
 
-				bind = SUPER, H, exec, sh /etc/aether/window-manager/scripts/toggle-floating-focus.sh
+				bind = SUPER, H, exec, /etc/aether/window-manager/scripts/toggle-floating-focus.sh
 				bind = SUPER SHIFT, H, togglefloating
 				bind = SUPER SHIFT, H, centerwindow, 1
 
